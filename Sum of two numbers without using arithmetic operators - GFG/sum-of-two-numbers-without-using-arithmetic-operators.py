19d3b0395923fb416f14418fe298f8c61d5245f2
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-04-22 19:27:08 +0530

    Added solution - LeetHub

---
+++ Sum of two numbers without using arithmetic operators - GFG/sum-of-two-numbers-without-using-arithmetic-operators.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+def sum(a,b):
+    #code here
+    return a+b
+#{ 
+#  Driver Code Starts
+import math
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        inp = list(map(int,input().split())) 
+        
+        a=inp[0]
+        b=inp[1]
+        
+        #ob=Solution()
+        print(sum(a,b))
+     
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
