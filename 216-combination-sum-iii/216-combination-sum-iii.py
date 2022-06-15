86df52c9daa1879737258919b2aab8df215e0738
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-06-15 21:46:17 +0530

    Time: 41 ms (61.47%), Space: 13.9 MB (77.36%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,28 +1,32 @@
-from copy import deepcopy
+from copy import copy
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         return (self.help(1, [], [], k, 0, n))
         
     def help(self, i, tmp, ans, k, s, n):
         
-        if k < 0:
-            return 
+       
+        if s > n:
+            return
+        if len(tmp) > k:
+            return
         
-        if s == n:
-            if k == 0:
-                ans.append(deepcopy(tmp))
+        if s == n and len(tmp) == k:
+            ans.append(copy(tmp))
             return
         if i == 10:
             return
         
-        # Pick it
+        
+        # skip it
+        self.help(i+1, tmp, ans, k, s, n)
+        
         tmp.append(i)
         s += i
-        self.help(i+1, tmp, ans, k-1, s, n)
-        tmp.pop()
-        s -= i
-        # Leave it
+        
         self.help(i+1, tmp, ans,k,s,n)
         
-       
+        tmp.pop()
+        s -= i
+        
         return ans
