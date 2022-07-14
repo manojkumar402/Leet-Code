39935f872c6715a31c1920488d23db64906634e3
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-07-14 10:39:56 +0530

    Time: 37 ms (84.30%), Space: 13.8 MB (53.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,13 +1,26 @@
 class Solution:
     def fib(self, n: int) -> int:
-        self.dp = [-1 for i in range(n+1)]
+        # Bottom - up 
+        if n <= 1:
+            return n
+        dp = [-1 for i in range(n+1)]
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, len(dp)):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
         
-        def fibo(n)->int:
-            if n == 0 or n == 1:
-                return n
-            if self.dp[n] != -1:
-                return self.dp[n]
-            else:
-                self.dp[n] =  fibo(n-1) + fibo(n-2)
-                return self.dp[n]
-        return fibo(n)
+        
+        # Top - down Recursion + Memoization
+
+#         self.dp = [-1 for i in range(n+1)]
+        
+#         def fibo(n)->int:
+#             if n == 0 or n == 1:
+#                 return n
+#             if self.dp[n] != -1:
+#                 return self.dp[n]
+#             else:
+#                 self.dp[n] =  fibo(n-1) + fibo(n-2)
+#                 return self.dp[n]
+#         return fibo(n)
