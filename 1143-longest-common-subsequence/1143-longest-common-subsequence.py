eaf5a16724aaa15cf1f5d7a6a35937a7fddedb11
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-07-17 13:44:24 +0530

    Time: 1862 ms (8.06%), Space: 24.6 MB (29.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        self.dp = [[None for i in range(len(text2)+1)]for j in range(len(text1)+1)]
-        # intialization 
+        self.dp = [[None for i in range(len(text2))]for j in range(len(text1))]
+        a =  self.rec(text1, text2, len(text1), len(text2))
         # print(self.dp)
-        return self.rec(text1, text2, len(text1), len(text2))
-        
+        return a
+    # recursion + memoization 
     def rec(self, X, Y, n, m):
         if n == 0 or m == 0:
             return 0
@@ -17,3 +17,13 @@ class Solution:
             else:
                 self.dp[n-1][m-1] = max(self.rec(X, Y, n-1,m),self.rec(X, Y, n,m-1))
                 return self.dp[n-1][m-1]
+        
+    # recursion 
+#     def rec(self, X, Y, n, m):
+#         if n == 0 or m == 0:
+#             return 0
+        
+#         if X[n-1] == Y[m-1]:
+#             return 1 + self.rec(X, Y, n-1,m-1)
+#         else:
+#             return max(self.rec(X, Y, n-1,m),self.rec(X, Y, n,m-1))
