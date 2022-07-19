e6078a5f001d0b7bddea90056c9afcd098d740a2
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-07-19 16:53:47 +0530

    Time: 49 ms (50.05%), Space: 14 MB (19.08%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         dp = [-1 for i in range(len(nums))]
-        return self.solve(nums, len(nums)-1, dp)
-    def solve(self, nums, n, dp):
-        if n < 0:
+        return self.solve(nums, 0, dp)
+    def solve(self, nums, i, dp):
+        if i >= len(nums):
             return 0
         
-        if n == 0:
-            return nums[n]
-        if dp[n] != -1:
-            return dp[n]
+        if i == len(nums) - 1:
+            return nums[i]
+        if dp[i] != -1:
+            return dp[i]
         # pick it 
-        incl = self.solve(nums, n-2, dp) + nums[n]
-        excl = self.solve(nums, n-1,dp)
-        dp[n] = max(incl, excl)
-        return dp[n]
+        incl = self.solve(nums, i+2, dp) + nums[i]
+        excl = self.solve(nums, i+1,dp)
+        dp[i] = max(incl, excl)
+        return dp[i]
