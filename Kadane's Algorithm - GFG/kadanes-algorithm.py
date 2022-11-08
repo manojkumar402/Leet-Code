a17775fb4ca568b06f1d5aed790ea7e3f4480770
Manoj Kumar <57716055+manojkumar402@users.noreply.github.com>
2022-11-08 20:30:39 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.py
+++ Kadane's Algorithm - GFG/kadanes-algorithm.py
@@ -6,15 +6,18 @@ class Solution:
     def maxSubArraySum(self,arr,N):
         ##Your code here
         curr_sum = 0
-        max_sum = float('-inf')
+        max_sum = arr[0]
         for i in range(N):
             curr_sum += arr[i]
             max_sum = max(max_sum, curr_sum)
             if curr_sum < 0:
                 curr_sum = 0
+           
         return max_sum
+
+
 #{ 
-#  Driver Code Starts
+ # Driver Code Starts
 #Initial Template for Python 3
 
 import math
